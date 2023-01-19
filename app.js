a6e559b3475ca8055f986d025bbc9d981adea37f
Tero Keränen <tero-keranen@hotmail.com>
2023-01-19 08:26:21 +0200

    index.html sisältö, app.js starfield ja css tyylitiedosto

---
+++ app.js
@@ -0,0 +1,76 @@
+function starField (getSpeed, height,width) {
+
+
+let canvas = document.getElementById('stars');
+canvas.width = width;
+canvas.height = height;
+
+let c = canvas.getContext("2d");
+let numStars = 150;
+let stars = [];
+let size  = 1;
+let fl = canvas.width;
+let centerX = canvas.width / 2;
+let centerY = canvas.height / 2;
+let speed = getSpeed;
+for (let i = 0; i < numStars; i++) {
+    stars[i] = new Star();
+}
+
+
+function Star () {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random () * canvas.height;
+    this.z = Math.random () * canvas.width;
+
+
+    this.move = function () {
+        this.z = this.z-speed;
+        if (this.z <= 0) {
+            this.z = canvas.width;
+        }
+    }
+    this.show = function () {
+
+        let x,y,s;
+
+        x = (this.x - centerX) * (fl/this.z);
+        x = x + centerX;
+
+        y = (this.y - centerY) * (fl/this.z);
+        y = y+centerY
+
+        s = size * (fl/this.z);
+
+        c.beginPath();
+        c.fillStyle = "white";
+        c.arc(x,y,s,0,Math.PI*2);
+        c.fill();
+    }
+
+};
+
+
+function draw () {
+
+    c.fillStyle = "black";
+    c.fillRect(0,0,canvas.width,canvas.height);
+    for (let i = 0; i < numStars; i++) {
+    stars[i].show();
+    stars[i].move();
+}
+    
+
+
+
+}
+
+function update () {
+    draw();
+    window.requestAnimationFrame(update);
+}
+
+update();
+}
+
+
